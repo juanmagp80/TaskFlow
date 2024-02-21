7a275f48fffb8f4a3d8f248d21763a5a0307e573
Juanma Garrido <juanmagpdev@gmail.com>
2024-02-21 09:41:32 +0100

    first commit

---
+++ components/logo.tsx
@@ -0,0 +1,23 @@
+import { Link } from "lucide-react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="TaskFlow" width={30} height={30} />
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          TaskFlow
+        </p>
+      </div>
+    </Link>
+  );
+};
