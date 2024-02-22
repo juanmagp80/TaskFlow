eef529f443a007160d36e576daeaa0d25c5373d0
Juanma Garrido <juanmagpdev@gmail.com>
2024-02-22 09:45:47 +0100

    varios cambios en footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -2,21 +2,21 @@ import { Logo } from "@/components/logo";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-const Navbar = () => {
+const Footer = () => {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <div className="fixed bottom-0 w-full p-4 border-t  bg-slate-100 flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between w-full">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="outline" asChild>
-            <Link href="/login">Login</Link>
+          <Button size="sm" variant="ghost">
+            Privacy Policy
           </Button>
-          <Button size="sm" asChild>
-            <Link href="/signup">Probar TaskFlow gratis</Link>
+          <Button size="sm" variant="ghost">
+            Terms of service
           </Button>
         </div>
       </div>
     </div>
   );
 };
-export default Navbar;
+export default Footer;
---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "TaskFlow",
+  description:
+    "Colabora, gestiona y haz seguimiento de tus proyectos de forma sencilla.",
+
+  // Add your site domain here
+};
